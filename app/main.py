0dad0d7e24d4d6b3b145238aed0d116945d71543
Oleh M <oleh.murachov28@gmail.com>
2026-03-27 06:55:23 +0100

    p

--- app/main.py
+++ app/main.py
@@ -40,11 +40,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        # ❗ НИЧЕГО НЕ ДОБАВЛЯЕМ — MRO уже всё сделал
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
